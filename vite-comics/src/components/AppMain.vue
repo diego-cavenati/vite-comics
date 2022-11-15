a01166c6d85795f7c27cf08d77b9af460c778993
diego-cavenati <diegocavenati@gmail.com>
2022-11-15 15:51:14 +0100

    Add component header

---
+++ vite-comics/src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: 'AppMain',
+}
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            Content goes here
+        </div>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
