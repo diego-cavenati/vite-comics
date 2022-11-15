82a36d04d47aa6094d760e7a1f3195a8b55359f9
diego-cavenati <diegocavenati@gmail.com>
2022-11-15 16:45:03 +0100

    add footer

--- vite-comics/src/components/AppMain.vue
+++ vite-comics/src/components/AppMain.vue
@@ -1,14 +1,22 @@
 <script>
+import Banner from './Banner.vue';
 export default {
     name: 'AppMain',
+    components: {
+        Banner
+    }
 }
 </script>
 
 <template>
     <main>
         <div class="container">
-            Content goes here
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam tenetur fugiat esse facere dicta iure
+                quis nam ullam velit vero quam architecto fuga vitae, qui minus iusto cupiditate expedita assumenda!</p>
         </div>
+
+        <Banner />
+
     </main>
 </template>
 
